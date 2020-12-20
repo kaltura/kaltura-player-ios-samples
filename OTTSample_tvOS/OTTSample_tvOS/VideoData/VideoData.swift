e857a7daee9b6ec068505740c27c00b02519e310
Nilit Danan <nilit.danan@kaltura.com>
2020-12-20 16:30:43 +0200

    Added OTTSample_tvOS

---
+++ OTTSample_tvOS/OTTSample_tvOS/VideoData/VideoData.swift
@@ -0,0 +1,69 @@
+//
+//  VideoData.swift
+//  OTTSample_tvOS
+//
+//  Created by Nilit Danan on 12/20/20.
+//
+
+import PlayKit
+import PlayKitProviders
+import KalturaPlayer
+
+struct PlayerData {
+
+    var autoPlay: Bool = true
+    var preload: Bool = true
+    
+    var ks: String?
+    
+    var pluginConfig: PluginConfig?
+    
+    init(autoPlay: Bool = true, preload: Bool = true, ks: String? = nil, pluginConfig: PluginConfig? = nil) {
+        self.autoPlay = autoPlay
+        self.preload = preload
+        self.ks = ks
+        self.pluginConfig = pluginConfig
+    }
+}
+
+struct OTTMedia {
+    var ks: String?
+    var assetId: String
+    var assetType: AssetType
+    var assetReferenceType: AssetReferenceType
+    var playbackContextType: PlaybackContextType
+    var formats: [String]?
+    var fileIds: [String]?
+    var networkProtocol: String?
+    var urlType: String?
+    var streamerType: String?
+    var startTime: TimeInterval?
+    
+    func mediaOptions() -> OTTMediaOptions {
+        let ottMediaOptions = OTTMediaOptions()
+        
+        ottMediaOptions.ks = ks
+        ottMediaOptions.assetId = assetId
+        ottMediaOptions.assetType = assetType
+        ottMediaOptions.assetReferenceType = assetReferenceType
+        ottMediaOptions.formats = formats
+        ottMediaOptions.fileIds = fileIds
+        ottMediaOptions.playbackContextType = playbackContextType
+        ottMediaOptions.networkProtocol = networkProtocol
+        ottMediaOptions.urlType = urlType
+        ottMediaOptions.streamerType = streamerType
+        
+        if let startTime = startTime {
+            ottMediaOptions.startTime = startTime
+        }
+        
+        return ottMediaOptions
+    }
+}
+
+struct VideoData {
+    var title: String
+    
+    var player: PlayerData
+    var media: OTTMedia
+}
