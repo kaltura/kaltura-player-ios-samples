5909db774226565261f8f4686faefd90c28abf9f
Sergey Chausov <chausov@users.noreply.github.com>
2021-06-18 15:38:04 +0300

    iOS, tvOS  platforms fixes

--- travis-build.sh
+++ travis-build.sh
@@ -12,42 +12,42 @@ keepAlive() {
 buildBasicSampleiOS() {
   echo Building the KalturaPlayer iOS BasicSample test App
   cd BasicSample
-  xcodebuild clean build -workspace BasicSample.xcworkspace -scheme BasicSample ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone X' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
+  xcodebuild clean build -workspace BasicSample.xcworkspace -scheme BasicSample ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone 11' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
   cd ../
 }
 
 buildBasicSampletvOS() {
   echo Building the KalturaPlayer tvOS BasicSample test App
   cd BasicSample_tvOS
-  xcodebuild clean build -workspace BasicSample_tvOS.xcworkspace -scheme BasicSample_tvOS ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone X' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
+  xcodebuild clean build -workspace BasicSample_tvOS.xcworkspace -scheme BasicSample_tvOS ONLY_ACTIVE_ARCH=NO -destination 'platform=tvOS Simulator,name=Apple TV' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
   cd ../
 }
 
 buildOTTSampleiOS() {
   echo Building the KalturaPlayer iOS OTTSample test App
   cd OTTSample
-  xcodebuild clean build -workspace OTTSample.xcworkspace -scheme OTTSample ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone X' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
+  xcodebuild clean build -workspace OTTSample.xcworkspace -scheme OTTSample ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone 11' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
   cd ../
 }
 
 buildOTTSampletvOS() {
   echo Building the KalturaPlayer tvOS OTTSample test App
   cd OTTSample_tvOS
-  xcodebuild clean build -workspace OTTSample_tvOS.xcworkspace -scheme OTTSample_tvOS ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone X' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
+  xcodebuild clean build -workspace OTTSample_tvOS.xcworkspace -scheme OTTSample_tvOS ONLY_ACTIVE_ARCH=NO -destination 'platform=tvOS Simulator,name=Apple TV' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
   cd ../
 }
 
 buildOVPSampleiOS() {
   echo Building the KalturaPlayer iOS OVPSample test App
   cd OVPSample
-  xcodebuild clean build -workspace OVPSample.xcworkspace -scheme OVPSample ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone X' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
+  xcodebuild clean build -workspace OVPSample.xcworkspace -scheme OVPSample ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone 11' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
   cd ../
 }
 
 buildOVPSampletvOS() {
   echo Building the KalturaPlayer tvOS OVPSample test App
   cd OVPSample_tvOS
-  xcodebuild clean build -workspace OVPSample_tvOS.xcworkspace -scheme OVPSample_tvOS ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone X' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
+  xcodebuild clean build -workspace OVPSample_tvOS.xcworkspace -scheme OVPSample_tvOS ONLY_ACTIVE_ARCH=NO -destination 'platform=tvOS Simulator,name=Apple TV' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
   cd ../
 }
 
