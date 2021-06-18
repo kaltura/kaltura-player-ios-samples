79dd69d2bd307380ec7445444242a85814a915c8
Sergey Chausov <chausov@users.noreply.github.com>
2021-06-18 16:43:39 +0300

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -12,7 +12,7 @@ keepAlive() {
 buildBasicSampleiOS() {
   echo Building the KalturaPlayer iOS BasicSample test App
   cd BasicSample
-  xcodebuild clean build -workspace BasicSample.xcworkspace -scheme BasicSample -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
+  xcodebuild clean build -workspace BasicSample.xcworkspace -scheme BasicSample -sdk iphonesimulator ONLY_ACTIVE_ARCH=YES | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
   cd ../
 }
 
