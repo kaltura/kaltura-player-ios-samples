ca7d24fef1b1834e65c1b7c18a9e256b8197c240
Sergey Chausov <chouzoff@gmail.com>
2021-06-24 23:18:42 +0300

    travis-build.sh updated

--- travis-build.sh
+++ travis-build.sh
@@ -9,59 +9,36 @@ keepAlive() {
   done
 }
 
-buildBasicSampleiOS() {
-  echo Building the KalturaPlayer iOS BasicSample test App
-  cd BasicSample
-  xcodebuild clean build -workspace BasicSample.xcworkspace -scheme BasicSample -destination 'platform=iOS Simulator,name=iPhone 11' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
-  cd ../
-}
-
-buildBasicSampletvOS() {
-  echo Building the KalturaPlayer tvOS BasicSample test App
-  cd BasicSample_tvOS
-  xcodebuild clean build -workspace BasicSample_tvOS.xcworkspace -scheme BasicSample_tvOS -destination 'platform=tvOS Simulator,name=Apple TV' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
-  cd ../
-}
-
-buildOTTSampleiOS() {
-  echo Building the KalturaPlayer iOS OTTSample test App
-  cd OTTSample
-  xcodebuild clean build -workspace OTTSample.xcworkspace -scheme OTTSample -destination 'platform=iOS Simulator,name=iPhone 11' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
-  cd ../
-}
-
-buildOTTSampletvOS() {
-  echo Building the KalturaPlayer tvOS OTTSample test App
-  cd OTTSample_tvOS
-  xcodebuild clean build -workspace OTTSample_tvOS.xcworkspace -scheme OTTSample_tvOS -destination 'platform=tvOS Simulator,name=Apple TV' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
-  cd ../
-}
-
-buildOVPSampleiOS() {
-  echo Building the KalturaPlayer iOS OVPSample test App
-  cd OVPSample
-  xcodebuild clean build -workspace OVPSample.xcworkspace -scheme OVPSample -destination 'platform=iOS Simulator,name=iPhone 11' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
-  cd ../
-}
-
-buildOVPSampletvOS() {
-  echo Building the KalturaPlayer tvOS OVPSample test App
-  cd OVPSample_tvOS
-  xcodebuild clean build -workspace OVPSample_tvOS.xcworkspace -scheme OVPSample_tvOS -destination 'platform=tvOS Simulator,name=Apple TV' | tee xcodebuild.log | xcpretty -r html && exit ${PIPESTATUS[0]}
-  cd ../
-}
-
 FLAG=$(mktemp)
+CODE=0
 
 if [ -n "$TRAVIS_TAG" ] || [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
   keepAlive $FLAG &
 else
-  buildBasicSampleiOS
-  buildBasicSampletvOS
-  buildOTTSampleiOS
-  buildOTTSampletvOS
-  buildOVPSampleiOS
-  buildOVPSampletvOS
+  rm -rf build
+
+  for dir in */ ; do
+    cd $dir
+    
+    TARGET_NAME=$(echo "$dir" | rev | cut -c2- | rev)
+    WORKSPACE="$TARGET_NAME.xcworkspace"
+
+    DESTINATION="platform=iOS Simulator,name=iPhone 11,OS=latest"
+    
+    if [ "$dir" == *"_tvOS"* ]; then
+      DESTINATION="platform=tvOS Simulator,name=Apple TV,OS=latest"
+    fi
+    
+    echo "Will start xcodebuild for workspace: $WORKSPACE, scheme: $TARGET_NAME, destination $DESTINATION"
+
+    xcodebuild clean build -workspace $WORKSPACE -scheme $TARGET_NAME -destination "$DESTINATION" | tee xcodebuild.log | xcpretty -r html; CODE=${PIPESTATUS[0]}
+    
+    echo "$TARGET_NAME xcodebuild was finished with status: $CODE"
+
+    if [ $CODE -ne 0 ]; then cd ..; exit $CODE; fi
+    cd ../
+  done
+
 fi
 
-rm $FLAG  # stop keepAlive
+rm $FLAG # stop keepAlive
