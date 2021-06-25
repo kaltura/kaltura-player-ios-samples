63c198392459a40127a188d25fe9b53f0ffb3ed3
Sergey Chausov <chouzoff@gmail.com>
2021-06-25 12:58:30 +0300

    Readme and travis build minor updates

--- travis-build.sh
+++ travis-build.sh
@@ -29,11 +29,11 @@ else
       DESTINATION="platform=tvOS Simulator,name=Apple TV,OS=latest"
     fi
     
-    echo "Will start xcodebuild for workspace: $WORKSPACE, scheme: $TARGET_NAME, destination $DESTINATION"
+    echo "⏰ Will start xcodebuild for workspace: $WORKSPACE, scheme: $TARGET_NAME, destination $DESTINATION"
 
     xcodebuild clean build -workspace $WORKSPACE -scheme $TARGET_NAME -destination "$DESTINATION" | tee xcodebuild.log | xcpretty -r html; CODE=${PIPESTATUS[0]}
     
-    echo "$TARGET_NAME xcodebuild was finished with status: $CODE"
+    echo "🎉 $TARGET_NAME xcodebuild was finished with status: $CODE"
 
     if [ $CODE -ne 0 ]; then cd ..; exit $CODE; fi
     cd ../
