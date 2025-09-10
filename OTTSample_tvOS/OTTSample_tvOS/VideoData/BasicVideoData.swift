d441989193073c73720a196ecd0b8705a7d98d17
Viktor <>
2025-09-10 18:22:04 +0300

    tvOS: New test environment (partner id 5033)

--- OTTSample_tvOS/OTTSample_tvOS/VideoData/BasicVideoData.swift
+++ OTTSample_tvOS/OTTSample_tvOS/VideoData/BasicVideoData.swift
@@ -10,47 +10,52 @@ extension VideoData {
     static func getBasicVideos() -> [VideoData] {
         var videos: [VideoData] = []
         
-        videos.append(VideoData(title: "548575, autoPlay-true, preload-true",
+        videos.append(VideoData(title: "3835484, autoPlay-true, preload-true",
                                 player: PlayerData(autoPlay: true,
                                                    preload: true),
-                                media: OTTMedia(assetId: "548575",
+                                media: OTTMedia(assetId: "3835484",
                                                 assetType: .media,
                                                 assetReferenceType: .media,
                                                 playbackContextType: .playback,
-                                                networkProtocol: "http")))
+                                                networkProtocol: "https",
+                                                urlType: "DIRECT")))
         
-        videos.append(VideoData(title: "548570, autoPlay, preload, startTime-20",
+        videos.append(VideoData(title: "3835484, autoPlay, preload, startTime-20",
                                 player: PlayerData(),
-                                media: OTTMedia(assetId: "548570",
+                                media: OTTMedia(assetId: "3835484",
                                                 assetType: .media,
                                                 assetReferenceType: .media,
                                                 playbackContextType: .playback,
-                                                networkProtocol: "http",
+                                                networkProtocol: "https",
+                                                urlType: "DIRECT",
                                                 startTime: 20)))
         
-        videos.append(VideoData(title: "548576, autoPlay-false, preload",
+        videos.append(VideoData(title: "3835484, autoPlay-false, preload",
                                 player: PlayerData(autoPlay: false),
-                                media: OTTMedia(assetId: "548576",
+                                media: OTTMedia(assetId: "3835484",
                                                 assetType: .media,
                                                 assetReferenceType: .media,
                                                 playbackContextType: .playback,
-                                                networkProtocol: "http")))
+                                                networkProtocol: "https",
+                                                urlType: "DIRECT")))
         
-        videos.append(VideoData(title: "548577, autoPlay, preload-false",
+        videos.append(VideoData(title: "3835484, autoPlay, preload-false",
                                 player: PlayerData(preload: false),
-                                media: OTTMedia(assetId: "548577",
+                                media: OTTMedia(assetId: "3835484",
                                                 assetType: .media,
                                                 assetReferenceType: .media,
                                                 playbackContextType: .playback,
-                                                networkProtocol: "http")))
+                                                networkProtocol: "https",
+                                                urlType: "DIRECT")))
 
-        videos.append(VideoData(title: "548551, autoPlay-false, preload-false",
+        videos.append(VideoData(title: "3835484, autoPlay-false, preload-false",
                                 player: PlayerData(autoPlay: false, preload: false),
-                                media: OTTMedia(assetId: "548551",
+                                media: OTTMedia(assetId: "3835484",
                                                 assetType: .media,
                                                 assetReferenceType: .media,
                                                 playbackContextType: .playback,
-                                                networkProtocol: "http")))
+                                                networkProtocol: "https",
+                                                urlType: "DIRECT")))
         
         return videos
     }
